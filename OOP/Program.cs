c76669a79fe19eddfb30b81cc476cf8cf2557063
hqk0611 <khaihq19@uef.edu.vn>
2023-11-06 11:10:56 +0700

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Animal
+    {
+        public double Weight;
+        public double Height;
+        public void Info()
+        {
+            Console.WriteLine("Height: " + Height + "\tWeight: " + Weight);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Example
+
+            Animal Dog = new Animal();
+            Dog.Weight = 2; // Assign values ​​to object properties
+            Dog.Height = 50;
+
+            Animal Cat = new Animal();
+            Cat.Weight = 1;
+            Cat.Height = 30;
+            
+            Dog.Info(); //Call the object's methods
+            Cat.Info();
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
+//What is OOP?
+/// OOP stands for Object-Oriented Programming.
+/// The four basic principles of object-oriented programming are:
+/// Abstraction: Modeling the relevant attributes and interactions of entities as classes to define an abstract representation of a system.
+/// Encapsulation: Hiding the internal state and functionality of an object and only allowing access through a public set of functions.
+/// Inheritance: Ability to create new abstractions based on existing abstractions.
+/// Polymorphism: Ability to implement inherited properties or methods in different ways across multiple abstractions
+
+/// Accessibility:
+/// public
+/// protected
+/// private
+/// private protected
+/// internal
+/// protected internal
+/// The default accessibility is private
+
+
---
+++ Try_Catch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    int[] myNumbers = { 1, 2, 3 };
+            //    Console.WriteLine(myNumbers[10]);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+
+            //    //You can also output your own error message:
+            //    Console.WriteLine("Something went wrong.");
+            //}
+
+            ////The finally statement lets you execute code, after try...catch, regardless of the result:
+            //finally 
+            //{
+            //    Console.WriteLine("The 'try catch' is finished.");
+            //}  
+            
+            //Console.ReadKey();
+        }
+    }
+}
+
+//C# Exceptions:
+///When executing C# code, different errors can occur: coding errors made by the programmer, errors due to wrong input, or other unforeseeable things.
+///When an error occurs, C# will normally stop and generate an error message. The technical term for this is: C# will throw an exception (throw an error).
+
+//C# try and catch:
+///The try statement allows you to define a block of code to be tested for errors while it is being executed.
+///The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+///The try and catch keywords come in pairs:
+/// try
+/// {
+///     Block of code to try
+/// }
+/// catch (Exception e)
+/// {
+/// Block of code to handle errors
+/// }
+
+
+
+
+
